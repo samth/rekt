9347ec119cf53ad5f13e729f666bca501b2b1eda
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2016-12-17 17:57:57 -0500

    REKT

--- main.rkt
+++ main.rkt
@@ -1,10 +1,15 @@
 #lang racket
 (provide #%module-begin #%top-interaction)
 (define-syntax-rule (#%module-begin . _)
-  (#%plain-module-begin ((error-display-handler) "REKT" 'REKT)))
+  (#%plain-module-begin ((error-display-handler) "REKT" "REKT")))
 
 (define-syntax-rule (#%top-interaction . _)
-  ((error-display-handler) "REKT" 'REKT))
+  (error "REKT"))
 
-(module reader syntax/module-reader
-  rekt)
+(module reader racket
+  (require (prefix-in r: racket))
+  (provide read read-syntax)
+  (define (read-syntax . args)
+    (r:read-syntax 'REKT (open-input-string "(module m rekt)")))
+  (define (read . args)
+    (r:read (open-input-string "(module m rekt)"))))
